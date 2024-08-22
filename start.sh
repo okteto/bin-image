177231dff0722dffc4d59c1fc74031c8646f8842
Javier Lopez <javier@okteto.com>
2024-08-22 14:25:44 +0200

    fix: race condition
    Signed-off-by: Javier Lopez <javier@okteto.com>

--- start.sh
+++ start.sh
@@ -63,5 +63,10 @@ while getopts ":s:ervd" opt; do
         esac
 done
 
+syncthingHome=/var/syncthing
+log "Copying configuration files to $syncthingHome"
+cp /var/syncthing/secret/* $syncthingHome
+chmod 644 $syncthingHome/cert.pem $syncthingHome/config.xml $syncthingHome/key.pem
+
 log "Executing okteto-supervisor $remote $reset $verbose"
 exec /var/okteto/bin/okteto-supervisor $remote $reset $verbose
